37eb672b3acac87696aa102c5dbcb5cfffcf2a5a
morapet <peter@moran.sk>
2020-03-29 17:20:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,15 @@ mkdir -p testapp/build
 sudo apt-get install gcc libpq-dev -y
 sudo apt-get install python-dev  python-pip -y
 sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+
+rm -rf env
+mkdir -p env
+
+python3 -m pip install --user virtualenv
+python3 -m venv env
+
+source env/bin/activate
+
 pip3 install setuptools
 pip3 install wheel
 pip3 install conan
