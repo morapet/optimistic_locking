74acc6bff0597423433affd183a42e42536a0687
morapet <peter@moran.sk>
2020-03-29 17:12:13 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ set -x
 rm -rf testapp/build
 mkdir -p testapp/build
 
-pip install wheel
-python setup.py bdist_wheel 
-pip install conan
+sudo apt-get install gcc libpq-dev -y
+sudo apt-get install python-dev  python-pip -y
+sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+pip3 install wheel
+pip3 install conan
 
 conan install ..
 cmake ../src/ -DCMAKE_BUILD_TYPE=Release
